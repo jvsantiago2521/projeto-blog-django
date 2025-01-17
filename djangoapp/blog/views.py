0d8699d36e4ad0a7cd36a3377e92a2efc96a5fe9
João Victor <joaovictor.santiago2521@gmail.com>
2025-01-17 18:16:54 -0300

    Commit Atualizando blog.

--- djangoapp/blog/views.py
+++ djangoapp/blog/views.py
@@ -1,6 +1,7 @@
 from django.core.paginator import Paginator
 from django.shortcuts import render
 from blog.models import Post
+from django.db.models import Q
 
 PER_PAGE = 9
 
@@ -20,7 +21,7 @@ def index(request):
     )
 
 
-def page(request):
+def page(request, slug):
     return render(
         request,
         'blog/pages/page.html',
@@ -30,11 +31,75 @@ def page(request):
     )
 
 
-def post(request):
+def post(request, slug):
+    post = Post.objects.get_published().filter(slug=slug).first()
+
     return render(
         request,
         'blog/pages/post.html',
         {
-            # 'page_obj': page_obj,
+            'post': post,
+        }
+    )
+
+def created_by(request, author_pk):
+    posts = Post.objects.get_published().filter(created_by__pk=author_pk)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def category(request, slug):
+    posts = Post.objects.get_published().filter(category__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def tag(request, slug):
+    posts = Post.objects.get_published().filter(tags__slug=slug)
+
+    paginator = Paginator(posts, PER_PAGE)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': page_obj,
+        }
+    )
+
+def search(request):
+    search_value = request.GET.get('search').strip()
+    posts = Post.objects.get_published().filter(
+        Q(title__icontains=search_value) |
+        Q(excerpt__icontains=search_value) |
+        Q(content__icontains=search_value) 
+        )[:PER_PAGE]
+
+    return render(
+        request,
+        'blog/pages/index.html',
+        {
+            'page_obj': posts,
+            'search_value': search_value,
         }
     )
